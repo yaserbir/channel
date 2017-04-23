4ada02293877edd9e970251a92cdf262773a3522
yaserbir <yaserbir@yahoo.com>
2017-04-23 20:22:38 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "346360276:AAFNtd9RluqsTyx5ZrLct7ro6KWuSzJoNk8"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 110638500
